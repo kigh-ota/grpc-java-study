63259fa9373f8b200e8e658031336eaf3bf7f749
Kaiichiro Ota <kigh.ota@gmail.com>
2021-09-18 22:44:49 +0900

    set deadline from client

--- src/main/java/client/GrpcClient.java
+++ src/main/java/client/GrpcClient.java
@@ -7,30 +7,33 @@ import io.grpc.Context;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.stub.StreamObserver;
 
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class GrpcClient {
     public static void main(String[] args) throws InterruptedException {
+        boot();
+    }
+
+    private static void boot() {
         var channel = ManagedChannelBuilder.forAddress("localhost", 8080).usePlaintext().build();
-
-        Context.current().withCancellation();
-
         var stub = ComputeGrpc.newStub(channel);
-        stub.boot(BootRequest.newBuilder().build(), new StreamObserver<>() {
-            @Override
-            public void onNext(BootResponse value) {
-                System.out.printf("Boot: %s\n", value.getMessage());
-            }
+        Context.current().withDeadlineAfter(2500, TimeUnit.MILLISECONDS, Executors.newScheduledThreadPool(0)).run(() -> {
+            stub.boot(BootRequest.newBuilder().build(), new StreamObserver<>() {
+                @Override
+                public void onNext(BootResponse value) {
+                    System.out.printf("Boot: %s\n", value.getMessage());
+                }
 
-            @Override
-            public void onError(Throwable t) {
-                System.err.print("receiving boot response: " + t.toString() + "\n");
-            }
+                @Override
+                public void onError(Throwable t) {
+                    System.err.print("receiving boot response: " + t.toString() + "\n");
+                }
 
-            @Override
-            public void onCompleted() {
-            }
+                @Override
+                public void onCompleted() {
+                }
+            });
         });
-        channel.awaitTermination(2500, TimeUnit.MILLISECONDS);
     }
 }
--- src/main/java/server/ComputeImpl.java
+++ src/main/java/server/ComputeImpl.java
@@ -15,6 +15,7 @@ public class ComputeImpl
             deepthought.BootRequest request,
             io.grpc.stub.StreamObserver<deepthought.BootResponse> responseObserver) {
         var context = Context.current();
+        System.err.printf("Deadline=%s\n", context.getDeadline());
         while (true) {
             if (context.isCancelled()) {
                 return;
@@ -43,6 +44,7 @@ public class ComputeImpl
         }
 
         var deadline = Context.current().getDeadline();
+        System.err.printf("Deadline=%s\n", deadline);
         if (deadline == null || deadline.timeRemaining(TimeUnit.MILLISECONDS) > 750) {
             var response = InferResponse.newBuilder().setAnswer(42).setDescription(0, "I checked it").build();
             responseObserver.onNext(response);
